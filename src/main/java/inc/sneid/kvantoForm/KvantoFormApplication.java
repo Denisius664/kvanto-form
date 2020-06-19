e8c0d0a9a44b2e75f87ed8b56b7b5ecebdce13b7
Denisius664 <denismir423@gmail.com>
2020-06-19 16:30:11 +1100

    First commit for start project

---
+++ src/main/java/inc/sneid/kvantoForm/KvantoFormApplication.java
@@ -0,0 +1,13 @@
+package inc.sneid.kvantoForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvantoFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KvantoFormApplication.class, args);
+	}
+
+}
